9efd10fff61fc57d6b4ed1c5a73b4ef01f6e3dbe
drewfeld216 <dfeld216@gmail.com>
2022-05-04 18:11:31 -0400

    Updates table and chart output templates

--- flaskr/data.py
+++ flaskr/data.py
@@ -9,39 +9,56 @@ from flaskr.db import get_db
 from flaskr.auth import login_required, admin_required
 
 import pandas as pd
+from sqlalchemy.sql import select
+
 
 bp = Blueprint('data', __name__, url_prefix='/data')
 
 @bp.route('/chart', methods=('GET', 'POST'))
 def chart():
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('data/chart.html', values=values, labels=labels, legend=legend)
+    from flaskr.models import Article, Topic
+    db = get_db()
+
+    articles = pd.read_sql(db.query(Article).statement, db.bind)
+    topics = pd.read_sql(db.query(Topic).statement, db.bind)
+
+    articles = articles.join(topics.set_index('id'), how='left', on='topic_id', lsuffix='l', rsuffix='r')
+    articles_by_topic = articles.groupby('name').count()
+
+    return render_template('data/chart.html', type='bar', 
+                            values=articles_by_topic['id'], labels=articles_by_topic.index, legend='Articles')
 
 @bp.route('/articles', methods=('GET', 'POST'))
 def articles():
-    from flaskr.models import Article
+    from flaskr.models import Article, Topic, Author
     db = get_db()
-    articles = db.query(Article)
-    heads = list(articles[0].__dict__.keys())
-    print(heads)
-    return render_template('data/table.html', values=articles, headers=heads)
+
+    articles = pd.read_sql(db.query(Article).statement, db.bind)
+    topics = pd.read_sql(db.query(Topic).statement, db.bind)
+    authors = pd.read_sql(db.query(Author).statement, db.bind)
+
+    articles = articles.join(topics.set_index('id'), how='left', on='topic_id', lsuffix='l', rsuffix='r')
+    articles = articles.join(authors.set_index('id'), how='left', on='author_id', lsuffix='l', rsuffix='r')
+    
+    heads_select=['day', 'namel', 'namer', 'wordcount']
+    heads_repl = ['Publication Date', 'Topic', 'Author', 'Word Count']
+    articles = articles[heads_select]
+    articles = articles.rename(columns={i:j for i, j in zip(heads_select, heads_repl)})
+
+    return render_template('data/table.html', table=articles)
 
 @bp.route('/users', methods=('GET', 'POST'))
 def users():
     from flaskr.models import User
     db = get_db()
-    users = db.query(User)
-    heads = list(users[0].__dict__.keys())
-    print(heads)
-    return render_template('data/table.html', values=users, headers=heads)
+    users = pd.read_sql(db.query(User).statement, db.bind)
+
+    return render_template('data/table.html', table=users)
 
 @bp.route('/pvs', methods=('GET', 'POST'))
 def pvs():
     from flaskr.models import Pageview
     db = get_db()
-    pvs = db.query(Pageview)
-    heads = list(pvs[0].__dict__.keys())
-    print(heads)
-    return render_template('data/table.html', values=pvs, headers=heads)
+    pvs = pd.read_sql(db.query(Pageview ).statement, db.bind)
+
+    return render_template('data/table.html', table=pvs)
